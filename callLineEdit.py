3a3117906b631f0a3950f864cf815e379d0c5760
Pan alek <ali.hatbi@wp.pl>
2023-10-11 12:06:44 +0200

    GUI UPDATED

--- callLineEdit.py
+++ callLineEdit.py
@@ -1,10 +1,27 @@
 import sys
 
-from PyQt6.QtWidgets import QDialog
+from PyQt6 import uic
+from PyQt6.QtWidgets import QDialog, QApplication
 
 from ELO import *
+
 
 class MyForm(QDialog):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Dialog
+
+        self.ui = uic.loadUi("ELO.ui", self)
+        #self.ui = Ui_Dialog()
+        #.ui.setupUi(self)
+        self.ui.ButtonClick.clicked.connect(self.dispmessage)
+        self.show()
+
+    def dispmessage(self):
+        self.ui.label.setText("masz " + self.ui.lineEditIQ.text() + " iq XD")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyForm()
+    window.show()
+    sys.exit(app.exec())
